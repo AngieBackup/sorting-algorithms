9785bbaea0fb2da74e5c73dac2683619155a763d
Angelica014 <angelica@fostertech.me>
2020-11-23 01:18:01 -0400

    BubbleSort Add

---
+++ BubbleSort.py
@@ -0,0 +1,25 @@
+# Python program for implementation of Bubble Sort 
+
+def bubbleSort(array): 
+    n = len(array) 
+  
+    # Traverse through all array elements 
+    for i in range(n): 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if array[j] > array[j+1] : 
+                array[j], array[j+1] = array[j+1], array[j]
+
+# Driver code to test above 
+array = [64, 34, 25, 12, 22, 11, 90] 
+  
+bubbleSort(array) 
+  
+print ("Sorted array is:") 
+for i in range(len(array)): 
+    print ("%d" %array[i]),
